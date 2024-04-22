e9f2bb0c2a57c057ef03596c74e3fb54e53f88bd
Saksham Tushar <sakshamtushar@gmail.com>
2024-04-22 12:20:00 +0000

    Added Logstash Configuration for Splunk HEC in Configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -9,11 +9,14 @@ INDEX="main"
 ENDPOINT="${SPLUNK_URL}/services/data/inputs/http/"
 
 # Create the JSON payload for the HEC collector
-curl -k -u admin:$PASSWORD $SPLUNK_URL/servicesNS/nobody/search/data/inputs/http -d name=elastic-input -d index=main -d indexes=main #| grep token | cut -d '>' -f 2  | cut -d '<' -f 1
+curl -k -u admin:$PASSWORD $SPLUNK_URL/servicesNS/nobody/search/data/inputs/http -d name=elastic-input -d index=main -d indexes=main 
 if echo "$response" | grep -q '"token"'; then
     # Extract the token from the response
-    token=$(echo "$response" | grep -oP '"token":"\K[^"]+')
+    token=$(echo "$response" | | grep token | cut -d '>' -f 2  | cut -d '<' -f 1)
     echo "HEC token created successfully: $token"
+    echo "Token=Splunk $token" >> .env
+
 else
     echo "Failed to create HEC collector: $response"
+    exit
 fi
