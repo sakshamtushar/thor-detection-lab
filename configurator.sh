2d125739d2f2b660d52f8b03677d37d4842ee742
Saksham Tushar <sakshamtushar@gmail.com>
2024-04-24 04:42:00 +0000

    logstash-splunk integration

--- configurator.sh
+++ configurator.sh
@@ -1,22 +1,23 @@
 
 #!/bin/bash
 
-
-#docker-compose -f spllunk-compose_v2.yml up -d
+#docker-compose -f splunk-compose_v2.yml up -d
 SPLUNK_URL="https://192.168.0.108:8089"
 PASSWORD="thor-lab@123"
 INDEX="main" 
 ENDPOINT="${SPLUNK_URL}/services/data/inputs/http/"
 
 # Create the JSON payload for the HEC collector
-curl -k -u admin:$PASSWORD $SPLUNK_URL/servicesNS/nobody/search/data/inputs/http -d name=elastic-input -d index=main -d indexes=main 
+response=$(curl -k -u admin:$PASSWORD $SPLUNK_URL/servicesNS/nobody/search/data/inputs/http -d name=elastic-input-test -d index=main -d indexes=main)
 if echo "$response" | grep -q '"token"'; then
     # Extract the token from the response
-    token=$(echo "$response" | | grep token | cut -d '>' -f 2  | cut -d '<' -f 1)
+    token=$(echo "$response" | grep token | cut -d '>' -f 2  | cut -d '<' -f 1)
     echo "HEC token created successfully: $token"
-    echo "Token=Splunk $token" >> .env
+    echo "SPLUNK_TOKEN=$token" >> .env
 
 else
     echo "Failed to create HEC collector: $response"
     exit
 fi
+
+./elastic-container.sh start
