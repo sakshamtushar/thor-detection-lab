7163faf9cbbd61256cf3c468e7f866405e2a9be6
sakshamtushar <sakshamtushar@gmail.com>
2024-04-26 15:06:02 +0530

    memory usage optimization & Splunk-logstash-testing

--- configurator.sh
+++ configurator.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+#sudo apt-get install docker-compose
+docker-compose -f splunk-compose_v2.yml up -d
 
-#docker-compose -f splunk-compose_v2.yml up -d
-SPLUNK_URL="https://192.168.0.108:8089"
+get_health_status() {
+    docker inspect --format='{{.State.Health.Status}}' "splunk-thorlab"
+}
+while true; do
+    health_status=$(get_health_status)
+    if [[ "$health_status" == "healthy" ]];  # Check if health status is "healthy"
+    then
+        echo "Container 'splunk-thorlab' is healthy."
+        break
+    else
+        echo "Waiting for container 'splunk' to become healthy (current status: $health_status)."
+        # Wait for 5 seconds before checking again
+        sleep 5
+    fi
+done
+
+echo "configuring splunk HEC & Disabling HEC Global SSL Configuration..."
+SPLUNK_URL="https://192.168.0.138:8089"
 PASSWORD="thor-lab@123"
 INDEX="main" 
 ENDPOINT="${SPLUNK_URL}/services/data/inputs/http/"
@@ -10,7 +28,7 @@ ENDPOINT="${SPLUNK_URL}/services/data/inputs/http/"
 curl -k -u admin:$PASSWORD $SPLUNK_URL/servicesNS/nobody/search/data/inputs/http/http -d  enableSSL=0
 # Create the JSON payload for the HEC collector
 response=$(curl -k -u admin:$PASSWORD $SPLUNK_URL/servicesNS/nobody/search/data/inputs/http -d name=elastic-input-test -d index=main -d indexes=main)
-if echo "$response" | grep -q '"token"'; then
+if echo "$response" | grep -E -q 'token|already'; then
     # Extract the token from the response
     token=$(echo "$response" | grep token | cut -d '>' -f 2  | cut -d '<' -f 1)
     echo "HEC token created successfully: $token"
@@ -20,5 +38,5 @@ else
     echo "Failed to create HEC collector: $response"
     exit
 fi
-
+echo "Setting up & Configuring Elastic"
 ./elastic-container.sh start
